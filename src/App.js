c9ce48397856dd3509b3c86de8cabc7960d15b73
Pat Palmerston <patpalmerston@gmail.com>
2019-07-02 12:57:24 -0700

    initial set up for Navbar

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react'; // instead of using React.Component for class component you add the this to the import
+import Navbar from './components/layout/Navbar';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <Navbar />
+      </div>
+    ); // We removed the props and used static default props on the Navbar component instead. Any props made here will override the static default props <Navbar title='Whatever' icon='fab fa-github' />
+  }
 }
 
 export default App;
+// Look up fragments
+// look up using the snippets
+//with out jsx using just vanilla JS
+/*
+render() {
+  return React.createElement(
+    'div',
+    {className: 'App'},
+    Rect.creatElement('h1', null, 'Hello from React')
+    );
+}
+*/
---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export class Navbar extends Component {
+  /*
+  you can use static props instead of setting props in app.js
+  */
+  static defaultProps = {
+    title: 'Github Finder',
+    icon: 'fab fa-github'
+  };
+
+  static propTypes = {
+    title: PropTypes.string.isRequired,
+    icon: PropTypes.string.isRequired
+  };
+
+  render() {
+    return (
+      <nav className='navbar bg-primary'>
+        <h1>
+          <i className={this.props.icon} /> {this.props.title}
+        </h1>
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
