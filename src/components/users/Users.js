add1af9f183689adcca01fc98e78d86262961dc9
Pat Palmerston <patpalmerston@gmail.com>
2019-07-03 14:00:33 -0700

    added proptypes to users

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import UserItem from './UserItem';
 import Spinner from '../layout/Spinner';
+import PropTypes from 'prop-types';
 
 const Users = ({ users, loading }) => {
   if (loading) {
@@ -14,6 +15,11 @@ const Users = ({ users, loading }) => {
       </div>
     );
   }
+};
+
+Users.propTypes = {
+  users: PropTypes.array.isRequired,
+  loading: PropTypes.bool.isRequired
 };
 
 const userStyle = {
