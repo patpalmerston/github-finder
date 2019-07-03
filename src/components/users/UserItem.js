95b31716344fd61dfa3bfe2485a795d9ddd815d4
Pat Palmerston <patpalmerston@gmail.com>
2019-07-03 11:24:17 -0700

    done restructuring userItem with proptypes

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const UserItem = ({ user: { login, avatar_url, html_url } }) => {
   return (
@@ -18,6 +19,10 @@ const UserItem = ({ user: { login, avatar_url, html_url } }) => {
       </div>
     </div>
   );
+};
+
+UserItem.propTypes = {
+  user: PropTypes.object.isRequired
 };
 
 export default UserItem;
